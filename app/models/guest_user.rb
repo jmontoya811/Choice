3c458c9097b4c5690a9a51d2ce67b2d92806082f
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-11-27 22:14:15 -0700

    created index/new/create methods for portfolios

--- app/models/guest_user.rb
+++ app/models/guest_user.rb
@@ -5,12 +5,5 @@ class GuestUser < User
     super || guest_user
   end
 
-  def guest_user
-    guest = GuestUser.new
-    guest.name = "Guest User"
-    guest.first_name = "Guest"
-    guest.last_name = "User"
-    guest.email = "guest@example.com"
-    guest 
-  end
+  
 end
