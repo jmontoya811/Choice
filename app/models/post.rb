187e606a743e3b6454bcf5a9f4ca0d9bf1e98cc4
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-11-29 13:36:00 -0700

    implemented data validations in models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
     enum status: { pending: 0, approved: 1 }
     extend FriendlyId
     friendly_id :title, use:  :slugged 
+
+    validates_presence_of :title, :name, :age, :body, :class_desired
 end
