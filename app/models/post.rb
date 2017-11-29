71b2ad7ed0b60521f26daa06c6f1f94124ccc70e
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-11-29 13:17:15 -0700

    added slugs and status enum to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    enum status: { pending: 0, approved: 1 }
+    extend FriendlyId
+    friendly_id :title, use:  :slugged 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128232844) do
+ActiveRecord::Schema.define(version: 20171129201403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,9 @@ ActiveRecord::Schema.define(version: 20171128232844) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "class_desired"
+    t.integer "status", default: 0
+    t.string "slug"
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
   create_table "skills", force: :cascade do |t|
